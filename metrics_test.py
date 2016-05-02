ecd0fdbeec053fccff203b8c56a7d6c10256a366
IraKorshunova <i.hmarynka@gmail.com>
2016-05-02 11:52:38 +0200

    roc plot

--- metrics_test.py
+++ metrics_test.py
@@ -3,7 +3,7 @@ import numpy as np
 import sklearn.metrics
 from kaggle_auc import auc
 from sklearn_alternative_auc import _auc
-
+import matplotlib.pyplot as plt
 
 csv_filepath = 'answer.csv'
 clip2label, clip2usage = {}, {}
@@ -42,3 +42,17 @@ for u in usage:
     print u, 'Sklearn AUC', sklearn.metrics.roc_auc_score(targets, predictions)
     print u, 'Alternative sklearn AUC', _auc(targets, predictions)
     print '--------------------------'
+
+    fpr, tpr, _ = sklearn.metrics.roc_curve(targets, predictions)
+    roc_auc = sklearn.metrics.auc(fpr, tpr)
+
+    plt.figure()
+    plt.plot(fpr, tpr, label='ROC curve (area = %0.4f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.show()
